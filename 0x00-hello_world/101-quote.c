4a8d24c1c31f81d097ecf89218185f503e99d130
DoHaytham <elghandour86@gmail.com>
2023-03-20 16:01:18 -0700

    My Initial Commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
  */
 int main(void)
 {
-char *message = "and that piece of art is useful\" - \
-		 Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, message, 38);
+char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, msg, 38);
 return (1);
 }
