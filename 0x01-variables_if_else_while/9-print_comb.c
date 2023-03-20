e5dd1abfbfa48ba22c0bcec073e06843d2664598
DoHaytham <elghandour86@gmail.com>
2023-03-20 15:48:40 -0700

    My Initial Commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,23 +7,18 @@
 
 int main(void)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-	for (j = i; j < 10; j++)
-	{
-	putchar(i + '0');
-	putchar(j + '0');
-		if (i != 9 || j != 9)
+		putchar(i + '0');
+	if (i != 9)
 	{
 		putchar(',');
 		putchar(' ');
 	}
 	}
-	}
+		putchar('\n');
 
-	putchar('\n');
-
-	return 0;
+	return (0);
 }
