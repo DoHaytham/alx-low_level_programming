31bb759f7c74086a5f2cb6a2c9cc78cac961d144
DoHaytham <elghandour86@gmail.com>
2023-03-20 11:19:14 -0700

    My Initial Commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: 'This program prints the sizes of ome data types'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char *message = "and that piece of art is useful\" - \
+	    Dora Korpar, 2015-10-19\n";
+    write(STDERR_FILENO, message, 38);
+    return 1;
+}
