791096c9d60d5c5abb9c7b180f05564cea2b9b22
DoHaytham <elghandour86@gmail.com>
2023-03-24 18:01:04 -0700

    My Initial Commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_most_numbers - Function
+* Description: 'Multiply two integers'
+* Return: Nothing (Success)
+*/
+
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if ( i == 2 || i == 4 )
+			continue;
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
