1176415de20613dd948ba115797fb8807bbf6533
DoHaytham <elghandour86@gmail.com>
2023-03-24 17:34:25 -0700

    My Initial Commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_numbers - Function
+* Description: 'Multiply two integers'
+* Return: Nothing (Success)
+*/
+
+void print_numbers(void)
+{
+	int i;
+	for (i=0; i<10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+int _putchar(char c);
 int main(void);
 int _isdigit(int c);
 int mul(int a, int b);
