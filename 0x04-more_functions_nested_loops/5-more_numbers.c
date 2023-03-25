6542155e8b097ebce40550763abd6c507fd667bd
DoHaytham <elghandour86@gmail.com>
2023-03-24 18:17:57 -0700

    My Initial Commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* more_numbers - Function
+* Description: 'Multiply two integers'
+* Return: Nothing (Success)
+*/
+
+void more_numbers(void)
+{
+	char i,j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
+		}
+	_putchar('\n');
+	}
+}
