13b5857c1f4a953c41768de448bb678707007fe7
DoHaytham <elghandour86@gmail.com>
2023-03-28 18:57:47 -0700

    My Initial Commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+* _puts - Function
+* Description: 'prints a string, followed by a new line, to stdout'
+* @str : the character
+* Return: Nothing (Success)
+*/
+
+void _puts(char *str);
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
+}
