1db4215c9067ecdba5c44b3ea0ec648581c830f3
DoHaytham <elghandour86@gmail.com>
2023-03-28 18:52:35 -0700

    My Initial Commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strlen - Function
+* Description: 'returns the length of a string'
+* @s : the character
+* Return: Nothing (Success)
+*/
+
+int _strlen(char *s)
+{
+	int length = 0;
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return length;
+}
