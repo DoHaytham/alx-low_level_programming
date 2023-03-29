a6f2bc01de6c2bc012aa1fb6fce0e2623bebec20
DoHaytham <elghandour86@gmail.com>
2023-03-28 18:33:15 -0700

    My Initial Commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 * reset_to_98 - Function
 * Description: 'takes a pointer to an int as parameter and updates the value
 * it points to to 98'
-* @*n: The pointer
+* @n: The pointer
 * Return: Nothing (Success)
 */
 
